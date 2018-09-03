d7b9d783967347d22ca60522d78943b6ff57e76f
Pavel Korolev <dev@borodust.org>
2018-09-03 23:18:21 +0300

    Add .travis.yml build script

--- dist.lisp
+++ dist.lisp
@@ -1,5 +1,7 @@
 (cl:in-package :cl-user)
 
+(load (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname)))
+
 (ql:quickload :quickdist)
 
 (defun extract-arguments ()
@@ -8,6 +10,8 @@
 (destructuring-bind (dist-name tar projects-dir dists-dir &rest args)
     (extract-arguments)
   (declare (ignore args))
+  (format t "~&DIST NAME: ~A~&TAR: ~A~&PROJECT DIR: ~A~&DIST DIR: ~A"
+          dist-name tar projects-dir dists-dir)
   (let ((quickdist:*gnutar* tar))
     (quickdist:quickdist :name dist-name
                          :base-url "http://bodge.borodust.org/dist/"
