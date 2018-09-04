04b62ae57c427ec9e34273eece5a6b9a74e587a8
Pavel Korolev <dev@borodust.org>
2018-09-04 08:57:52 +0300

    Try loading missing systems

--- dist.lisp
+++ dist.lisp
@@ -2,7 +2,7 @@
 
 (load (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname)))
 
-(ql:quickload :quickdist)
+(ql:quickload '(:quickdist :cffi))
 
 (defun extract-arguments ()
   (uiop:command-line-arguments))
@@ -13,7 +13,12 @@
   (format t "~&DIST NAME: ~A~&TAR: ~A~&PROJECT DIR: ~A~&DIST DIR: ~A"
           dist-name tar projects-dir dists-dir)
   (let ((quickdist:*gnutar* tar))
-    (quickdist:quickdist :name dist-name
-                         :base-url "http://bodge.borodust.org/dist/"
-                         :projects-dir projects-dir
-                         :dists-dir dists-dir)))
+    (flet ((load-system (c)
+             (ql:quickload (asdf::missing-requires c) :prompt nil)
+             (quickdist:retry-loading-asd)))
+      (handler-bind ((asdf:missing-dependency #'load-system)
+                     (asdf:missing-component #'load-system))
+        (quickdist:quickdist :name dist-name
+                             :base-url "http://bodge.borodust.org/dist/"
+                             :projects-dir projects-dir
+                             :dists-dir dists-dir)))))
