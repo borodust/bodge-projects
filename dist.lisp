47520f13d2a4e452a1d9e3e41ece75159b1ece53
Pavel Korolev <dev@borodust.org>
2019-10-09 23:36:26 +0300

    Use .bodge directory

--- dist.lisp
+++ dist.lisp
@@ -1,24 +1,18 @@
-(cl:in-package :cl-user)
-
-(load (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname)))
+(script "ensure-quicklisp")
 
 (ql:quickload '(:quickdist :cffi))
 
-(defun extract-arguments ()
-  (uiop:command-line-arguments))
+(bind-parameters dist-name tar projects-dir dists-dir)
 
-(destructuring-bind (dist-name tar projects-dir dists-dir &rest args)
-    (extract-arguments)
-  (declare (ignore args))
-  (format t "~&DIST NAME: ~A~&TAR: ~A~&PROJECT DIR: ~A~&DIST DIR: ~A"
-          dist-name tar projects-dir dists-dir)
-  (let ((quickdist:*gnutar* tar))
-    (flet ((load-system (c)
-             (ql:quickload (asdf::missing-requires c) :prompt nil)
-             (quickdist:retry-loading-asd)))
-      (handler-bind ((asdf:missing-dependency #'load-system)
-                     (asdf:missing-component #'load-system))
-        (quickdist:quickdist :name dist-name
-                             :base-url "http://bodge.borodust.org/dist/"
-                             :projects-dir projects-dir
-                             :dists-dir dists-dir)))))
+(shout "~&DIST NAME: ~A~&TAR: ~A~&PROJECT DIR: ~A~&DIST DIR: ~A"
+       dist-name tar projects-dir dists-dir)
+(let ((quickdist:*gnutar* tar))
+  (flet ((load-system (c)
+           (ql:quickload (asdf::missing-requires c) :prompt nil)
+           (quickdist:retry-loading-asd)))
+    (handler-bind ((asdf:missing-dependency #'load-system)
+                   (asdf:missing-component #'load-system))
+      (quickdist:quickdist :name dist-name
+                           :base-url "http://bodge.borodust.org/dist/"
+                           :projects-dir projects-dir
+                           :dists-dir dists-dir))))
